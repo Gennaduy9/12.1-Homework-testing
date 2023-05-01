91d8b5d7493c837e2fe8745162c27a01616e817f
геннадий <gubingm@list.ru>
2023-05-01 15:47:52 +0300

    Ветка test/issue2

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,4 +1,7 @@
+import pytest
+
 from utils import arrs
+from utils.arrs import coating_types, divide
 
 
 def test_get():
@@ -9,3 +12,29 @@ def test_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+
+
+
+@pytest.mark.parametrize('array, coating_in_array, expcted', [
+    ([1, 2, 3], int, 3),
+    ([1, '2', '3'], str, 2),
+    ([1, '2', '3'], int, 1)
+
+])
+
+def test_coating_tupes(array, coating_in_array, expcted):
+    assert coating_types(array, coating_in_array) == expcted
+
+
+def test_divide():
+    assert divide(35, 7) == 5
+    assert divide(1_000_000, 2) == 500_000
+    assert divide(81, 9) == 9
+    assert divide(6.6, 2) == 3.3
+    assert divide(25, 5) == 5
+    assert divide(18, 3) == 6
+
+
+def test_divide_division_zro():
+    with pytest.raises(ZeroDivisionError):
+        divide(10, 0)
